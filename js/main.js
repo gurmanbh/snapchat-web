796144158132de93014dde39de61c433967e0fd6
gurmanbh <gurmanbhatia.93@gmail.com>
2015-02-17 08:14:12 -0500

    added more elements to the page. added jquery.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.fixme').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            bottom: 'auto'
+        });
+    } else {                                   // apply position: static
+        $('.fixme').css({                      // if you scroll above it
+            position: 'static'
+        });
+    }
+
+});
