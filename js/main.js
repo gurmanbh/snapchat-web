d291cf584ba93430840e53073d11b479f96e3f0c
Gurman Bhatia <gurmanbh@users.noreply.github.com>
2015-12-25 12:07:51 -0500

    removed comments

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ var timers = {
                         time: 3},
                 7: {ext: 'png',
                         time: 3},
-
         }
     },
 
@@ -41,7 +40,6 @@ var timers = {
                         time: 6},
                 4: {ext: 'png',
                         time: 4},
-
        }
     },
     snap3: {
@@ -58,8 +56,6 @@ var timers = {
                         time: 3},
                 3: {ext: 'gif',
                         time: 5},
-
-
     }
 }};
 
@@ -127,153 +123,6 @@ function machine(phn){
         }
 }
 
-// check page location
-
-/* $(window).scroll(function() { 
-    var buffer = 200;
-
-    var loc1 = $('#snap1').offset().top - buffer;
-    var loc2 = $('#snap2').offset().top - buffer;
-    var loc3 = $('#snap3').offset().top - buffer;
-    var loc4 = $('#snap4').offset().top - buffer;
-    var loc5 = $('#snap5').offset().top - buffer;
-
-    var page = $(window).scrollTop();  
-
-    if (page>loc1 && page<loc2){
-    machine ('snap1');
-    console.log('on snap 1');
-    }
-
-    if (page>loc2 && page<loc3){
-    machine ('snap2');
-    console.log('on snap 2');
-    }
-
-    if (page>loc3 && page<loc4){
-    machine ('snap3');
-    console.log('on snap 3');
-    }
-
-});
-*/
-//waypoints
-// html
-        //<div id="zero" class="iphone" data-which-phone="zero"></div>
-
-
-        // on scroll past phonefunction(){
-            //var which = $(this.element).attr('data-which-phone');
-            //var timer = setInterval(secondPassed(which))
-            //timers[which].timer = timer;
-        //})
-
-        // on pause
-            //var which = $(this.element).attr('data-which-phone');
-            //clearInterval(timers[which].timer);
-/*
-var datedata = jQuery.grep(bigdata, function(obj) {
-                return obj.thedate === strDateTime;
-                });
-
-                */
-/*
-var pw1 = new Waypoint({
-  element: document.getElementById("snap1"),
-  handler: function(direction) {
-    console.log('s-intro ' + direction);
-    if (direction == 'up'){
-        if (timers[phone].play== 'inactive'){
-            timers[phone].status='p';
-            machine(phone);
-            timers[phone].play= 'active'
-        } 
-        else {
-        timers[phone].status='p';
-        }}
-
-    }
-    if (direction == 'down'){
-        if (timers[phone].play== 'inactive'){
-            timers[phone].status='p';
-            machine(phone);
-            timers[phone].play= 'active'
-        } 
-        else {
-        timers[phone].status='p';
-        }}
-    }
-  },
-  offset: 'bottom-in-view'
-})
-
-var pw2 = new Waypoint({
-  element: document.getElementById("snap2"),
-  handler: function(direction) {
-    console.log('s-user-growth ' + direction);
-    if (direction == 'up'){
-        
-        machine ('snap2');
-    }
-    if (direction == 'down'){
-        timers.snap2.status = 'p';
-        timers.snap1.status = 'h';
-        machine ('snap2');
-    }
-  },
-  offset: 'bottom-in-view'
-})
-
-var pw3 = new Waypoint({
-  element: document.getElementById("snap3"),
-  handler: function(direction) {
-    console.log('s-interfaces ' + direction);
-    if (direction == 'up'){
-        timers.snap3.status = 'p';
-    }
-    if (direction == 'down'){
-        timers.snap2.status = 'h'
-        timers.snap1.status = 'p';
-        machine ('snap3');
-    }
-  },
-  offset: 'bottom-in-view'
-})
-
-var pw4 = new Waypoint({
-  element: document.getElementById("snap4"),
-  handler: function(direction) {
-    console.log('s-stories ' + direction);
-    if (direction == 'up'){
-        timers.snap4.status = 'p';
-    }
-    if (direction == 'down'){
-        timers.snap2.status = 'h'
-        timers.snap1.status = 'p';
-        machine ('snap4');
-    }
-  },
-  offset: 'bottom-in-view'
-})
-
-var pw5 = new Waypoint({
-  element: document.getElementById("snap5"),
-  handler: function(direction) {
-    if (direction == 'up'){
-        timers.snap5.status = 'p';
-    }
-    if (direction == 'down'){
-        timers.snap2.status = 'h'
-        timers.snap1.status = 'p';
-        machine ('snap5');
-    }
-  },
-  offset: 'bottom-in-view'
-})
-
-*/
-
-
 //make scroll sticky
 
 $(document).ready(function() {  
@@ -294,26 +143,6 @@ $(document).ready(function() {
     stickyNav();  
     });  
 });
-
-/*
-
-$(document).ready(function() {  
-var sticky = $('#s-intro .purplehead').offset().top;  
-var stickyNav = function(){  
-    var scrollTop = $(window).scrollTop();  
-
-    if (scrollTop > sticky) {   
-        $('#s-intro .purplehead').addClass('sticky1');
-    } else {  
-        $('#s-intro .purplehead').removeClass('sticky1');
-    }  
-    };  
-  
-    $(window).scroll(function() {  
-        stickyNav();  
-    });  
-});    
-*/
 
 //scroll to partcular section on the page
 $('.nav').on('click', function(){
